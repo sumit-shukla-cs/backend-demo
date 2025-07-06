8682cbf37ba153f989dfefbd38fea5e3e8140b81
Sumit Shukla <sumit.shukla0067@gmail.com>
2025-07-06 15:49:33 +0530

    github data added

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import "dotenv/config"; // Load environment variables from .env file
-const express = require("express");
+import ('dotenv').config()// Load environment variables from .env file
+const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
 });
